d493c8be8c0311494dbcac80415a42a9ef20a0a5
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-12-27 21:01:58 +0000

    buy me a coffee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
-import { Suspense } from 'react'
-import { DataTable } from '@/components/data-table'
-import { fetchReadmeContent } from '@/lib/github'
-import { DonationBox } from '@/components/donation-box'
+/* eslint-disable @next/next/no-img-element */
+import { Suspense } from "react";
+import { DataTable } from "@/components/data-table";
+import { fetchReadmeContent } from "@/lib/github";
+import Donation from "@/components/donation";
 
 export default async function Page() {
-  const data = await fetchReadmeContent()
-  
+  const data = await fetchReadmeContent();
+
   return (
     <div className="min-h-screen bg-zinc-950 text-zinc-50">
       <div className="sticky top-0 z-50 bg-emerald-500 p-2 text-center text-sm text-white">
-        This site is a repository on GitHub, and I parse the README in HTML format. To add something to the list,{' '}
-        <a 
-          href="https://github.com/birobirobiro/awesome-shadcn-ui" 
+        This site is a repository on GitHub, and I parse the README in HTML
+        format. To add something to the list,{" "}
+        <a
+          href="https://github.com/birobirobiro/awesome-shadcn-ui"
           className="underline hover:no-underline"
           target="_blank"
           rel="noopener noreferrer"
@@ -23,8 +25,8 @@ export default async function Page() {
       <div className="container mx-auto py-10 space-y-12">
         <div className="flex flex-col items-center justify-center space-y-4">
           <div className="w-32 h-32">
-            <img 
-              src="https://raw.githubusercontent.com/birobirobiro/awesome-shadcn-ui/64729b2c178e3fdcb42c0c7bf341bcde7ae502ea/assets/logo.svg" 
+            <img
+              src="https://raw.githubusercontent.com/birobirobiro/awesome-shadcn-ui/64729b2c178e3fdcb42c0c7bf341bcde7ae502ea/assets/logo.svg"
               alt="Awesome shadcn/ui logo"
               className="w-full h-full"
             />
@@ -34,9 +36,9 @@ export default async function Page() {
             A curated list of awesome things related to shadcn/ui
           </p>
           <div className="text-center text-sm text-zinc-400">
-            Created by:{' '}
-            <a 
-              href="https://birobirobiro.dev" 
+            Created by:{" "}
+            <a
+              href="https://birobirobiro.dev"
               className="text-zinc-200 hover:underline"
               target="_blank"
               rel="noopener noreferrer"
@@ -51,8 +53,7 @@ export default async function Page() {
         </Suspense>
       </div>
 
-      <DonationBox />
+      <Donation />
     </div>
-  )
+  );
 }
-
